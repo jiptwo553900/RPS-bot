8c4a047a0f81e4292c4a96b44b3a5241c51668d1
Maksim Lebedev <jiptwo.553900@gmail.com>
2023-06-23 13:47:40 +0300

    New: start the RPS project

---
+++ bot.py
@@ -0,0 +1,17 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+from config_data.config import Config, load_config
+
+# Инициализируем логгер
+logger = logging.getLogger(__name__)
+
+
+# Функция конфигурирования и запуска бота
+async def main():
+    # Конфигурируем логирование
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(filename)s:%(lineno)d #%(levelname)-8s "
+               "[%(asctime)s] - %(name)s - %(message)s")
